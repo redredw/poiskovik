4e5f2b4bbec48d12abf1a81a176ae7c7cdd03e59
redredw <98525817+redredw@users.noreply.github.com>
2022-01-27 14:12:00 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+setTimeout(function() {
+bl.style.display = 'block';
+loading.style.display = 'none';
+}, 2500);
+ function start (){
+      setTimeout(function() {
+   }, 200);
+       setTimeout(function() {
+  loading.style.display = 'block';
+       }, 600);
+   setTimeout(function() {
+  loading.style.display = 'none';
+$('#konteiner').fadeTo(500, 1); 
+}, 1900);   
+  }    
+ function otmena (){
+    konteiner.style.display = 'none'; 
+     bl.style.display = 'block'; 
+  }   
+  function fr (){
+    $('#konteiner2').fadeTo(500, 1); 
+     setTimeout(function() {
+konteiner2.style.display = 'none'; 
+}, 1500);   
+   }   
+ function fv (){
+    $('#konteiner3').fadeTo(500, 1); 
+     setTimeout(function() {
+konteiner3.style.display = 'none';
+}, 1500);   
+   } 
+   function ff (){
+    $('#konteiner3').fadeTo(500, 1); 
+     setTimeout(function() {
+ konteiner3.style.display = 'none';
+}, 1500);   
+   } 
+  function zak (){
+    z2.style.display = 'block'; 
+   z.style.display = 'none'; 
+   }   
+  function zak2 (){
+    z2.style.display = 'none'; 
+   z.style.display = 'block'; 
+  }   
